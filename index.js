3cb025b2c7ceaa68be3c3a5c19bf0035214d420d
Takahito Nakano <kano@ponko2.jp>
2016-06-18 23:25:25 +0900

    Lintにひっかかった部分を調整

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var Elixir = require('laravel-elixir');
 var objectAssign = require('object-assign');
 var uglifyOptions = Elixir.config.js.uglify.options;
 
-Elixir.config.js.uglify.options = objectAssign(uglifyOptions, {
-  preserveComments: 'license'
-});
+Elixir.config.js.uglify.options = objectAssign(
+  uglifyOptions,
+  {preserveComments: 'license'}
+);
 
-Elixir.config.js.browserify.plugins.push({
-  name: 'licensify'
-});
+Elixir.config.js.browserify.plugins.push({name: 'licensify'});
